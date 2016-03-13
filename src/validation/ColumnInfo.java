d287aa3d466fd27bd9928de1ba95369b1bbbe677
Cristian David Gonzalez Gonzalez <cristian_david_g@live.com>
2016-03-13 15:39:56 -0600

    ColumnInfo added

---
+++ src/validation/ColumnInfo.java
@@ -0,0 +1,28 @@
+package validation;
+
+public class ColumnInfo {
+   private final String columnName;
+   private final FieldValidator validator;
+   
+   public ColumnInfo(String columnName, FieldValidator validator) {
+      if (validator == null) throw new NullPointerException();
+      this.columnName = columnName;
+      this.validator  = validator;
+   }
+   
+   public boolean isNumeric() {
+      return validator instanceof NumericFieldValidator;
+   }
+   
+   public boolean isNominal() {
+      return validator instanceof NominalFieldValidator;
+   }
+   
+   public boolean validate(String value) {
+      return validator.validate(value);
+   }
+   
+   public String getColumnName() {
+      return columnName;
+   }
+}
