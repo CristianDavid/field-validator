bc183dd6111de022b1f0531993aded2436fbda26
Cristian David Gonzalez Gonzalez <cristian_david_g@live.com>
2016-04-17 21:21:26 -0500

    Report classes added

---
+++ src/reports/Correlation.java
@@ -0,0 +1,14 @@
+package reports;
+
+import org.apache.commons.csv.CSVRecord; 
+
+import validation.ColumnInfo;
+
+public class Correlation {
+   Correlation(ColumnInfo[] info, CSVRecord[] records) {
+      
+   }
+   void writeToDisk() {
+      
+   }
+}
---
+++ src/reports/Stats.java
@@ -0,0 +1,14 @@
+package reports;
+
+import org.apache.commons.csv.CSVRecord; 
+
+import validation.ColumnInfo;
+
+public class Stats {
+   Stats(ColumnInfo[] info, CSVRecord[] records) {
+      
+   }
+   void writeToDisk() {
+      
+   }
+}
