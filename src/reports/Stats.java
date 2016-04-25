86ab9367c01d20be220103960ca49fe0fb04b1c9
oscarAdrianMariscal <oscar.adrian.mariscal@gmail.com>
2016-04-24 23:16:24 -0500

    agregado la posibilidad de imprimir las frequencias para los atributos numericos

--- src/reports/Stats.java
+++ src/reports/Stats.java
@@ -15,13 +15,17 @@ import validation.NumericColumnInfo;
 public class Stats {
 	private static final Object [] FILE_HEADER_NUMERIC = {"Name","Max","Min","Mode","Mean","Std Deviation"};
 	private static final Object [] FILE_HEADER_NOMINAL = {"Name","Frequency"};
-	
+	ArrayList<ArrayList<FrequencyRecord<Double>>>  numericFrequencyRecords;
+	ArrayList<ArrayList<FrequencyRecord<String>>> nominalFrequencyRecords;
 	double  maximum [], minimum[],mode[],mean[],stdDeviation[];
-	ArrayList<ArrayList<String>> listsOfErrors;
-	ArrayList<ArrayList<String>> listsOfSuccesses;
-	ArrayList<ArrayList<Integer>> listsOfNumberOfSuccesses;
-	ArrayList<ArrayList<Integer>> listsOfNumberOfErrors;
 	ColumnInfo information[];
+	
+	//This is supposed to emulate a struct, to simplify code. 
+	class FrequencyRecord <T>{
+		public boolean isAnError;
+		public int frecuency;
+		public T value;
+	}
 
 	public Stats(ColumnInfo[] info, CSVRecord[] records) {
 		information = info;
@@ -30,11 +34,12 @@ public class Stats {
 		mode    = new double [information.length];
 		mean    = new double [information.length];
 		stdDeviation = new double [information.length];
-		listsOfNumberOfErrors    = new ArrayList<ArrayList<Integer>>();
-		listsOfNumberOfSuccesses = new ArrayList<ArrayList<Integer>>();
-		listsOfErrors    = new ArrayList<ArrayList<String>>();
-		listsOfSuccesses = new ArrayList<ArrayList<String>>();
+		numericFrequencyRecords = new ArrayList<>();
+		nominalFrequencyRecords = new ArrayList<>();
+
 		for (int i = 0 ; i< info.length; i++){
+			numericFrequencyRecords.add(new ArrayList<>()); 
+			nominalFrequencyRecords.add(new ArrayList<>()); 
 			if (info[i] instanceof NumericColumnInfo){
 			    NumericColumnInfo numInfo = (NumericColumnInfo) info[i];
 				int sum=0;
@@ -44,7 +49,7 @@ public class Stats {
 					if (numInfo.validate(value)){
 						double number=Integer.parseInt(value); 
 						sum+=number;
-						numbers.add(number);			
+						numbers.add(number);
 					}
 				}
 				mean[i]    = sum/records.length;
@@ -58,37 +63,50 @@ public class Stats {
 				numInfo.setMax(maximum[i]);
 				numInfo.setMean(mean[i]);
 				numInfo.setStandardDeviation(stdDeviation[i]);
+				//Getting the frequency
+				ArrayList<Double> temp=new ArrayList<>();
+				for (double number : numbers){
+					if (!temp.contains(number)){
+						FrequencyRecord<Double> record =new FrequencyRecord<>();
+						record.value=number;
+						record.isAnError=false;
+						record.frecuency=Collections.frequency(numbers, number);
+						numericFrequencyRecords.get(i).add(record);
+						temp.add(number);
+					}
+					
+				}
 			}
 			else{
-				ArrayList<String> errors = new ArrayList<String>();
-				ArrayList<Integer> numberOfErrors = new ArrayList<Integer>();
-				ArrayList<String> successes = new ArrayList<String>();
-				ArrayList<Integer> numberOfSuccesses = new ArrayList<Integer>();
-
+				ArrayList<String> successes = new ArrayList<>();
+				ArrayList<String> errors = new ArrayList<>();
 				ArrayList<String> list=new ArrayList<String>();
+				//Getting the frequency
 				for (int j = 1 ; j < records.length; j++){
 					String value =records[j].get(i).trim();
 					list.add(value); 
 					if (information[i].validate(value)){
 						if (!successes.contains(value)){
+							FrequencyRecord<String> frequencyRecord = new FrequencyRecord<>();
+							frequencyRecord.value= value;
+							frequencyRecord.isAnError=false;
+							nominalFrequencyRecords.get(i).add(frequencyRecord);
 							successes.add(value);
 						}
 					}
 					else{
 						if (!errors.contains(value)){
+							FrequencyRecord<String> frequencyRecord = new FrequencyRecord<>();
+							frequencyRecord.value= value;
+							nominalFrequencyRecords.get(i).add(frequencyRecord);
+							frequencyRecord.isAnError=true;
 							errors.add(value);							
 						}
-
 					}
 				}
-				for (String error : errors)
-					numberOfErrors.add(Collections.frequency(list, error));
-				for (String success: successes)
-					numberOfSuccesses.add(Collections.frequency(list, success));
-				listsOfErrors.add(errors); 
-				listsOfSuccesses.add(successes);
-				listsOfNumberOfSuccesses.add(numberOfSuccesses);
-				listsOfNumberOfErrors.add(numberOfErrors);
+				for (FrequencyRecord<String> record : nominalFrequencyRecords.get(i)){
+					record.frecuency=Collections.frequency(list, record.value);
+				}
 			}
 		}
 	}
@@ -108,7 +126,6 @@ public class Stats {
 				maxCount=Collections.frequency(a, a.get(i));
 			}
 		}
-
 		return maxValue;
 	}
 
@@ -130,31 +147,62 @@ public class Stats {
 					lst.add(String.valueOf(mean[i]));
 					lst.add(String.valueOf(stdDeviation[i]));
 					printer.printRecord(lst);
+					lst.clear();
 					printer.println();
+					
+					printer.printRecord("SUCCESSES");
+					for (int w = 0 ; w < numericFrequencyRecords.get(i).size(); w++){
+						if(!numericFrequencyRecords.get(i).get(w).isAnError){
+							String value=String.valueOf(numericFrequencyRecords.get(i).get(w).value);
+							String numberOfOcurrences=String.valueOf(numericFrequencyRecords.get(i).get(w).frecuency);						
+							lst.add(value);
+							lst.add(numberOfOcurrences);
+							printer.printRecord(lst);
+							lst.clear();							
+						}
+					}
+					printer.println();
+//					printer.printRecord("ERRORS");
+					for (int w = 0 ; w < numericFrequencyRecords.get(i).size(); w++){
+						if(numericFrequencyRecords.get(i).get(w).isAnError){
+							String value=String.valueOf(numericFrequencyRecords.get(i).get(w).value);
+							String numberOfOcurrences=String.valueOf(numericFrequencyRecords.get(i).get(w).frecuency);						
+							lst.add(value);
+							lst.add(numberOfOcurrences);
+							printer.printRecord(lst);
+							lst.clear();							
+						}
+					}
+					printer.println();
+					temp++;
 				}
 				else{
 					printer.printRecord("SUCCESSES");
 					printer.printRecord(FILE_HEADER_NOMINAL);
-					for (int w = 0 ; w < listsOfSuccesses.get(temp).size(); w++){
-						List<String> lst = new ArrayList<String>();
-						String value=listsOfSuccesses.get(temp).get(w);
-						String numberOfOcurrences=String.valueOf(listsOfNumberOfSuccesses.get(temp).get(w));						
-						lst.add(value);
-						lst.add(numberOfOcurrences);
-						printer.printRecord(lst);
-						lst.clear();
+					for (int w = 0 ; w < nominalFrequencyRecords.get(i).size(); w++){
+						if(!nominalFrequencyRecords.get(i).get(w).isAnError){
+							List<String> lst = new ArrayList<String>();
+							String value=nominalFrequencyRecords.get(i).get(w).value;
+							String numberOfOcurrences=String.valueOf(nominalFrequencyRecords.get(i).get(w).frecuency);						
+							lst.add(value);
+							lst.add(numberOfOcurrences);
+							printer.printRecord(lst);
+							lst.clear();							
+						}
 					}
 					printer.println();
 					printer.printRecord("ERRORS");
 					printer.printRecord(FILE_HEADER_NOMINAL);
-					for (int w = 0 ; w < listsOfErrors.get(temp).size(); w++){
-						List<String> lst = new ArrayList<String>();
-						String value=listsOfErrors.get(temp).get(w);
-						String numberOfOcurrences=String.valueOf(listsOfNumberOfErrors.get(temp).get(w));						
-						lst.add(value);
-						lst.add(numberOfOcurrences);
-						printer.printRecord(lst);
-						lst.clear();
+					for (int w = 0 ; w < nominalFrequencyRecords.get(i).size(); w++){
+						if(nominalFrequencyRecords.get(i).get(w).isAnError){
+							List<String> lst = new ArrayList<String>();
+							String value=nominalFrequencyRecords.get(i).get(w).value;
+							String numberOfOcurrences=String.valueOf(nominalFrequencyRecords.get(i).get(w).frecuency);						
+							lst.add(value);
+							lst.add(numberOfOcurrences);
+							printer.printRecord(lst);
+							lst.clear();							
+						}
 					}
 					printer.println();
 					temp++;
@@ -168,3 +216,4 @@ public class Stats {
 		} 
 	}
 }
+
