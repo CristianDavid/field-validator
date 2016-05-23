20891bc945735fc890cf6c30a2a094abf5ae630f
Cristian David Gonzalez Gonzalez <cristian_david_g@live.com>
2016-05-22 21:15:21 -0500

    Removed some warnings

--- src/validation/ColumnInfo.java
+++ src/validation/ColumnInfo.java
@@ -1,28 +1,20 @@
 package validation;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class ColumnInfo {
 	private final String columnName;
 	private final FieldValidator validator;
-	private ArrayList<String>  hits;
-	private ArrayList<Integer> hitFrequencies;
-	private ArrayList<String>  errors;
-	private ArrayList<Integer> errorFrequencies;
+//	private ArrayList<String>  hits;
+//	private ArrayList<Integer> hitFrequencies;
+//	private ArrayList<String>  errors;
+//	private ArrayList<Integer> errorFrequencies;
 
 	public ColumnInfo(String columnName, FieldValidator validator) {
 		if (validator == null) throw new NullPointerException();
 		this.columnName = columnName;
 		this.validator  = validator;
 	}
-
-	//public boolean isNumeric() {
-//		return validator instanceof NumericFieldValidator;
-	//}
-
-/*	public boolean isNominal() {
-		return validator instanceof NominalFieldValidator;
-	}*/
 
 	public boolean validate(String value) {
 		return validator.validate(value);
