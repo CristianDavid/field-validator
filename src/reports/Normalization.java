416072ae8964b8b1ea09cf1e1ae3f39b17c79318
oscarAdrianMariscal <oscar.adrian.mariscal@gmail.com>
2016-05-22 21:02:36 -0500

    I added new remaining two ways to do normalization

--- src/reports/Normalization.java
+++ src/reports/Normalization.java
@@ -23,6 +23,10 @@ public class Normalization {
 	}
 
 	public void writeToDisk(String filename) {
+		escalamientoDecimal(filename);
+	}
+	
+	public void minMaxNormalization(String filename){
 		try (PrintWriter outFile = new PrintWriter(filename);
 				CSVPrinter printer = new CSVPrinter(outFile, CSVFormat.EXCEL)) {
 			printer.printRecord(records[0]);
@@ -63,5 +67,100 @@ public class Normalization {
 			e.printStackTrace();
 		}
 	}
+	
+	public void zScore(String filename){
+		try (PrintWriter outFile = new PrintWriter(filename);
+				CSVPrinter printer = new CSVPrinter(outFile, CSVFormat.EXCEL)) {
+			printer.printRecord(records[0]);
+			for (int i = 0; i < records.length; i++) {
+				List<String> list = new ArrayList<String>();
+				// Checking for invalid row
+				boolean invalidRow = false;
+				for (int j = 0; j < records[j].size(); j++) {
+					if (information[j] instanceof NumericColumnInfo) {
+						if (!information[j].validate(records[i].get(j))) {
+							invalidRow = true;
+						}
+					}
+				}
+				if (invalidRow)
+					continue;
+				// printing if is valid
+				for (int j = 0; j < records[j].size(); j++) {
+					if (information[j] instanceof NumericColumnInfo) {
+						NumericColumnInfo info;
+						info = (NumericColumnInfo) information[j];
+						// Z-score Formula
+						Double value =  (Integer.parseInt(records[i].get(j)) -info.getMedian() )% info.getStandardDeviation();
+						list.add(String.valueOf(value));
+					} else {
+						list.add(records[i].get(j));
+					}
+				}
+				printer.printRecord(list);
+				list.clear();
 
+			}
+			printer.flush();
+			printer.close();
+		} catch (Exception e) {
+			System.out.println("Error in CsvFileWriter !!!");
+			e.printStackTrace();
+		}
+	}
+	
+	public void escalamientoDecimal(String filename){
+		try (PrintWriter outFile = new PrintWriter(filename);
+				CSVPrinter printer = new CSVPrinter(outFile, CSVFormat.EXCEL)) {
+			printer.printRecord(records[0]);
+			for (int i = 0; i < records.length; i++) {
+				List<String> list = new ArrayList<String>();
+				// Checking for invalid row
+				boolean invalidRow = false;
+				for (int j = 0; j < records[j].size(); j++) {
+					if (information[j] instanceof NumericColumnInfo) {
+						if (!information[j].validate(records[i].get(j))) {
+							invalidRow = true;
+						}
+					}
+				}
+				if (invalidRow)
+					continue;
+				// printing if is valid
+				for (int j = 0; j < records[j].size(); j++) {
+					if (information[j] instanceof NumericColumnInfo) {
+						NumericColumnInfo info;
+						info = (NumericColumnInfo) information[j];
+						// Formula de escalamiento decimal 
+						double maximunAbsoluteValue,divedeBy;
+						divedeBy =1;
+						if (Math.abs(info.getMax())>Math.abs(info.getMin())){
+							maximunAbsoluteValue =Math.abs(info.getMax());
+						}
+						else{
+							maximunAbsoluteValue =Math.abs(info.getMin());
+						}
+						double temp = maximunAbsoluteValue;
+						while (temp>1){
+							temp = maximunAbsoluteValue;
+							divedeBy*=10;
+							temp/=divedeBy;
+						}
+						Double value =  Float.parseFloat(records[i].get(j)) % divedeBy;
+						list.add(String.valueOf(value));
+					} else {
+						list.add(records[i].get(j));
+					}
+				}
+				printer.printRecord(list);
+				list.clear();
+
+			}
+			printer.flush();
+			printer.close();
+		} catch (Exception e) {
+			System.out.println("Error in CsvFileWriter !!!");
+			e.printStackTrace();
+		}
+	}
 }
